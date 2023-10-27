4e9204ed03e9b9516140b2c712a9ba3691265c55
Stefan Schreiter <ripper121@gmail.com>
2023-10-27 13:42:15 +0200

    v1.18
    - Moved Oscillate from config.txt to the PowderProfile (powder.txt)
    - added reverse to PowderProfile
    - fixed index.html
    - fixed code to work with Kern Scales
    - added Microsteps to config.txt
    - added DebugLog to config.txt

---
+++ Firmware/RoboTrickler/RoboTrickler_v1.18/flash.bat
@@ -0,0 +1,1 @@
+esptool.exe --chip esp32 --port COM7 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 ./RoboTrickler.ino.bootloader.bin 0x8000 ./RoboTrickler.ino.partitions.bin 0xe000 ./boot_app0.bin 0x10000 ./RoboTrickler.ino.bin
