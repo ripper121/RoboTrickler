8ebd94eabfd9370f911c1241c7d369c41c1583da
Stefan Schreiter <ripper121@gmail.com>
2024-01-15 20:43:49 +0100

    Added RS60 & profilGenerator Fixes

--- Firmware/SD-Files/Create_SD_Card_Batch.bat
+++ Firmware/SD-Files/Create_SD_Card_Batch.bat
@@ -1,8 +1,10 @@
 @echo off
 set /p driveLetter=Please enter your SD-Card Drive Letter:
+:loop
 format %driveLetter%: /FS:FAT32 /Q /V:R-Trickler /X
 xcopy /s/e/h/y/v .\SD-Files %driveLetter%:\
 echo -----
 echo Done!
 echo -----
 pause
+goto loop
