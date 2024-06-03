9d348aa93e3a301c2ac51c1e63c326daa43b5f07
Stefan S. <ripper121@gmail.com>
2024-06-03 07:53:24 +0200

    Latest Config and new Binarys

--- Firmware/SD-Files/Create_SD_Card.bat
+++ Firmware/SD-Files/Create_SD_Card.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p driveLetter=Please enter your SD-Card Drive Letter:
-format %driveLetter%: /FS:FAT32 /Q /V:R-Trickler /X
+format %driveLetter%: /FS:FAT32 /V:R-Trickler /X /A:1024 /Q
 xcopy /s/e/h/y/v .\SD-Files %driveLetter%:\
 echo -----
 echo Done!
